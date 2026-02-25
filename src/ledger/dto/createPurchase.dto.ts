07f01f13691badcd4176066bb4d71ef31a6a556e
Braijon <26751990+Solstice789@users.noreply.github.com>
2026-02-25 16:55:42 -0600

    Enhancement: Fix Dockerfile - Make swagger-ui testing more pleasant - update README accordingly. (#2)
    * Enhancement: Fix Dockerfile - Make testing more pleasant - update README info.
    
    * Update README add uuidv4 generator for testing.

--- src/ledger/dto/createPurchase.dto.ts
+++ src/ledger/dto/createPurchase.dto.ts
@@ -2,7 +2,10 @@ import { IsUUID } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePurchaseDto {
-  @ApiProperty({ format: 'uuid' })
+  @ApiProperty({
+    format: 'uuid',
+    example: 'fef3b13c-a0b2-4ba6-b13d-de56f5593144',
+  })
   @IsUUID()
   itemId!: string;
 }
